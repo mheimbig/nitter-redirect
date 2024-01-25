b3d047209bcc5972094f1df1318428cbd606e87f
Mark Heimbigner <96577+mheimbig@users.noreply.github.com>
2024-01-24 21:46:34 -0700

    Update background.js

--- background.js
+++ background.js
@@ -59,6 +59,8 @@ browser.webRequest.onBeforeRequest.addListener(
       "*://mobile.twitter.com/*",
       "*://pbs.twimg.com/*",
       "*://video.twimg.com/*",
+      "*://x.com/*",
+      "*://www.x.com/*",
     ],
   },
   ["blocking"]
